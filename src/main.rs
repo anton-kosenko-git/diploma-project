621fe77fe5615c125f0cd7c2f7111d8ae46cf121
Anton Kosenko <kosenko.mvp@gmail.com>
2022-07-10 13:26:57 +0300

    Creation a block structure and SHA-256 algorithm

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use blockchainlib::*;
+
+fn main() {
+    let mut block = Block::new(
+        0,
+        0,
+        vec![0; 32],
+        0,
+        "Genesis block".to_owned(),
+        0x0008ffffffffffffffffffffffffffff,
+    );
+
+    block.hash = block.hash();
+
+    println!("{:?}", &block);
+
+    block.mine();
+
+    println!("{:?}", &block);
+}
