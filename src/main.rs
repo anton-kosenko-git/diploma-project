f738cfe45797457c00fdafd734db498980e1ca20
Anton Kosenko <kosenko.mvp@gmail.com>
2022-07-10 17:13:18 +0300

    Adding a verification test

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,50 @@
 use blockchainlib::*;
 
 fn main() {
+    let difficulty = 0x000fffffffffffffffffffffffffffff;
+
     let mut block = Block::new(
         0,
-        0,
+        now(),
         vec![0; 32],
         0,
         "Genesis block".to_owned(),
-        0x0008ffffffffffffffffffffffffffff,
+        difficulty,
     );
-
-    block.hash = block.hash();
-
-    println!("{:?}", &block);
 
     block.mine();
 
-    println!("{:?}", &block);
+    println!("Mined genesis block {:?}", &block);
+
+    let mut last_hash = block.hash.clone();
+
+    let mut blockchain = Blockchain {
+        blocks: vec![block],
+    };
+
+    print!("Verify: {}", &blockchain.verify());
+
+    for i in 1..=10 {
+        let mut block = Block::new(
+            i,
+            now(),
+            last_hash,
+            0,
+            "Another block".to_owned(),
+            difficulty,
+        );
+
+        block.mine();
+
+        println!("Mined block {:?}", &block);
+
+        last_hash = block.hash.clone();
+
+        blockchain.blocks.push(block);
+
+        println!("Verify: {}", &blockchain.verify());
+    }
+
+    blockchain.blocks[3].prev_block_hash[18] = 8;
+    println!("Verify: {}", &blockchain.verify());
 }
